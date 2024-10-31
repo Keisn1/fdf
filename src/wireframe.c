60acfda3b3952cc867185f2fd288cc7eb742b9ad
Kay Freyer <kay.freyer@icloud.com>
2024-10-31 22:05:59 +0100

    refactored, need to scale still

--- src/wireframe.c
+++ src/wireframe.c
@@ -47,45 +47,11 @@ t_list	*get_wireframe_indices(size_t wf_m, size_t wf_n)
 	return (ret);
 }
 
-void	wf_to_img(t_mlx_data mlx_data, t_img img, t_mat M, t_list *wf)
-{
-	t_point	*idx;
-	t_list	*line;
-	t_list	*head;
-
-	while (wf)
-	{
-		idx = (t_point *)(wf->content);
-		line = get_bres_line((t_point){
-				(int)round(M.mat[0][idx->i]) + (img.width / 2),
-				(int)round(M.mat[1][idx->i]) + (img.height / 2)
-			},
-			(t_point){
-				(int)round(M.mat[0][idx->j]) + (img.width / 2),
-				(int)round(M.mat[1][idx->j]) + (img.height / 2)
-			} );
-		head = line;
-		while (head)
-		{
-			img_put_pixel(mlx_data.mlx_ptr, &img, *(t_point *)head->content,
-				0xFF00FF);
-			head = head->next;
-		}
-		ft_lstclear(&line, free);
-		wf = wf->next;
-	}
-}
-
-/* void	wf_to_img(t_mlx_data mlx_data, t_img img, t_mat M, t_map map) */
+/* void	wf_to_img(t_mlx_data mlx_data, t_img img, t_mat M, t_list *wf) */
 /* { */
+/* 	t_point	*idx; */
 /* 	t_list	*line; */
 /* 	t_list	*head; */
-
-
-/* 	size_t	count_m; */
-/* 	size_t	count_n; */
-/* 	size_t idx; */
-
 
 /* 	while (wf) */
 /* 	{ */
@@ -109,3 +75,63 @@ void	wf_to_img(t_mlx_data mlx_data, t_img img, t_mat M, t_list *wf)
 /* 		wf = wf->next; */
 /* 	} */
 /* } */
+
+void	wf_to_img(t_mlx_data mlx_data, t_img img, t_mat M, t_map map)
+{
+	t_list	*line;
+	t_list	*head;
+
+
+	size_t	count_m;
+	size_t	count_n;
+	size_t idx;
+
+	count_m = 0;
+	idx = 0;
+	while (count_m < map.m) {
+		count_n = 0;
+		while (count_n < map.n) {
+			if (count_n < map.n -1) {
+				line = get_bres_line((t_point){
+						(int)round(M.mat[0][idx]) + (img.width / 2),
+						(int)round(M.mat[1][idx]) + (img.height / 2)
+					},
+					(t_point){
+						(int)round(M.mat[0][idx+1]) + (img.width / 2),
+						(int)round(M.mat[1][idx+1]) + (img.height / 2)
+					} );
+
+				head = line;
+				while (head)
+				{
+					img_put_pixel(mlx_data.mlx_ptr, &img, *(t_point *)head->content,
+								  0xFF00FF);
+					head = head->next;
+				}
+				ft_lstclear(&line, free);
+			}
+			if (count_m < map.m-1) {
+				line = get_bres_line((t_point){
+						(int)round(M.mat[0][idx]) + (img.width / 2),
+						(int)round(M.mat[1][idx]) + (img.height / 2)
+					},
+					(t_point){
+						(int)round(M.mat[0][idx+map.n]) + (img.width / 2),
+						(int)round(M.mat[1][idx+map.n]) + (img.height / 2)
+					} );
+
+				head = line;
+				while (head)
+				{
+					img_put_pixel(mlx_data.mlx_ptr, &img, *(t_point *)head->content,
+								  0xFF00FF);
+					head = head->next;
+				}
+				ft_lstclear(&line, free);
+			}
+			count_n++;
+			idx++;
+		}
+		count_m++;
+	}
+}
