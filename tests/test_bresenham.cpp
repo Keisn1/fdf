b020b87224767a23a1992e3daa060c24626412ff
Kay Freyer <kfreyer@c2r4s2.42wolfsburg.de>
2024-12-15 15:37:26 +0100

    added tests

--- tests/test_bresenham.cpp
+++ tests/test_bresenham.cpp
@@ -69,7 +69,14 @@ INSTANTIATE_TEST_SUITE_P(
     bresPlotlineImgTemplateTest,
 	bresPlotlineImgTemplateTest,
 	testing::Values(
-		bresPlotlineImgTemplateTestParams{0, 0, 0, 3, { {0, 0}, {0, 1}, {0, 2}, {0, 3} }}
+		bresPlotlineImgTemplateTestParams{0, 0, 0, 3, { {0, 0}, {0, 1}, {0, 2}, {0, 3} }},
+		bresPlotlineImgTemplateTestParams {0, 0, 3, 0, {{0, 0}, {1, 0}, {2, 0}, {3, 0}}},
+		bresPlotlineImgTemplateTestParams {3, 0, 0, 0, {{3, 0}, {2, 0}, {1, 0}, {0, 0}}},
+		bresPlotlineImgTemplateTestParams {0, 3, 0, 0, {{0, 3}, {0, 2}, {0, 1}, {0, 0}}},
+		bresPlotlineImgTemplateTestParams {20, 20, 17, 17, {{20, 20}, {19, 19}, {18, 18}, {17, 17}}},
+		bresPlotlineImgTemplateTestParams {17, 20, 20, 17, {{17, 20}, {18, 19}, {19, 18}, {20, 17}}},
+		bresPlotlineImgTemplateTestParams {0, 1, 6, 4, {{0, 1}, {1, 2}, {2, 2}, {3, 3}, {4, 3}, {5, 4}, {6, 4}}},
+		bresPlotlineImgTemplateTestParams {6, 4, 0, 1, {{6, 4}, {5, 3}, {4, 3}, {3, 2}, {2, 2}, {1, 1}, {0, 1}}}
 		)
 	);
 
