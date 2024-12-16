89d12e2b21395ee2545cc369d988615089129b19
Kay Freyer <kay.freyer@icloud.com>
2024-12-16 18:13:24 +0100

    still working but close to solution

--- src/bresenham.c
+++ src/bresenham.c
@@ -45,54 +45,62 @@ t_line	new_line(t_pixel px1, t_pixel px2, int col1, int col2)
 	return (line);
 }
 
-int get_nbr_of_steps(t_line line)
+int	get_nbr_of_steps(t_line line)
 {
-	if (ft_abs((int)line.pixels[1].y - (int)line.pixels[0].y) < ft_abs((int)line.pixels[1].x - (int)line.pixels[0].x))
-		return  ft_abs((int)line.pixels[1].x - (int)line.pixels[0].x);
-	return ft_abs((int)line.pixels[1].y - (int)line.pixels[0].y);
+	if (ft_abs((int)line.pixels[1].y
+			- (int)line.pixels[0].y) < ft_abs((int)line.pixels[1].x
+			- (int)line.pixels[0].x))
+		return (ft_abs((int)line.pixels[1].x - (int)line.pixels[0].x));
+	return (ft_abs((int)line.pixels[1].y - (int)line.pixels[0].y));
 }
 
 // Function to extract RGB components
-void extractRGB(int color, int rgb[3]) {
+void	extractRGB(int color, float rgb[3])
+{
 	rgb[0] = (color >> 16) & 0xFF;
-	rgb[1]= (color >> 8) & 0xFF;
+	rgb[1] = (color >> 8) & 0xFF;
 	rgb[2] = color & 0xFF;
 }
 
-int calculate_color(t_line line, int steps, int step) {
-	// Extract initial RGB components
-	int rgb1[3];
-	int rgb2[3];
-	extractRGB(line.colors[0],rgb1);
+void	fill_grad_step(float grad_step[3], t_line line, int steps)
+{
+	float	rgb1[3];
+	float	rgb2[3];
+
+	extractRGB(line.colors[0], rgb1);
 	extractRGB(line.colors[1], rgb2);
+	grad_step[0] = (rgb2[0] - rgb1[0]) / (float)steps;
+	grad_step[1] = (rgb2[1] - rgb1[1]) / (float)steps;
+	grad_step[2] = (rgb2[2] - rgb1[2]) / (float)steps;
+}
 
+int	calculate_color(float color[3], float grad_step[3], int step)
+{
+	int	red;
+	int	green;
+	int	blue;
 
-	// Calculate step increments
-	float stepR = (rgb2[0] - rgb1[0]) / (float)steps;
-	float stepG = (rgb2[1] - rgb1[1]) / (float)steps;
-	float stepB = (rgb2[2] - rgb1[2]) / (float)steps;
-
-	int red = rgb1[0] + stepR * step;
-	int green = rgb1[1] + stepG * step;
-	int blue = rgb1[2] + stepB * step;
-
-	return (red << 16) | (green << 8) | blue;
+	red = color[0] + grad_step[0] * step;
+	green = color[1] + grad_step[1] * step;
+	blue = color[2] + grad_step[2] * step;
+	return ((red << 16) | (green << 8) | blue);
 }
 
 void	bres_plotline_img(t_mlx_data mlx_data, t_img *img, t_line line,
-		t_img_put_pixel_func t_img_put_pixel)
+		t_img_put_pixel_func img_put_pixel)
 {
 	t_bresenham	bres;
-	int count = 0;
-	int color;
-	int steps = get_nbr_of_steps(line);
+	int			step;
+	float		grad_step[3];
+	float		color[3];
 
+	step = 0;
+	extractRGB(line.colors[0], color);
+	fill_grad_step(grad_step, line, get_nbr_of_steps(line));
 	bres = new_bres(line.pixels[0], line.pixels[1]);
 	while (true)
 	{
-		color = calculate_color(line, steps, count);
-		count++;
-		t_img_put_pixel(mlx_data.mlx_ptr, img, line.pixels[0], color);
+		img_put_pixel(mlx_data.mlx_ptr, img, line.pixels[0], calculate_color(color, grad_step, step++));
 		bres.e2 = 2 * bres.err;
 		if (bres.e2 >= bres.dy)
 		{
