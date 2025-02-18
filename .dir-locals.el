1a8c2af1326cc77b1fa5a626cb457ec8a54ad460
Kay Freyer <kay.freyer@icloud.com>
2025-02-18 19:29:05 +0100

    add cover (but no cover yet)

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,10 @@
-((nil . ((org-roam-directory . "~/workspace/fdf/roam")
+((org-mode . ((eval . (add-hook 'after-save-hook
+                                (lambda nil
+                                  (when
+                                      (string-equal
+                                       (file-name-nondirectory buffer-file-name)
+                                       "README.org")
+                                    (org-pandoc-export-to-gfm)))
+                                nil t))))
+ (nil . ((org-roam-directory . "~/workspace/fdf/roam")
          (org-roam-db-location . "~/workspace/fdf/roam/fdf.db"))))
